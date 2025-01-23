0a517c3427b542d631feeac911d67d61ddac8710
UCSPavanKumar <pavanupadrasta@gmail.com>
2025-01-23 16:34:24 +0530

    backtest code enabled

---
+++ driver/startup.py
@@ -0,0 +1,11 @@
+mport yaml
+import sys
+fname = '/home/iob/algotrading/config/config.yaml'
+stream = open(fname,'r')
+data = yaml.safe_load(stream)
+data['auth_code'] = None
+data['access_token'] = None
+
+with open(fname,'w') as yaml_file:
+    yaml.dump(data,yaml_file,sort_keys=False)
+
